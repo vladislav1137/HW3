e4614e35ece962b8ec8385a9839f97f7f1327dc9
vladislav1137 <mr.vladislav220@yandex.ru>
2023-02-06 22:47:42 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,58 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+        task8();
+    }
+
+    public static void task1 () {
+        System.out.println("Задача 1");
+        byte q = -128;
+        System.out.println("Значение переменной q " + "с типом " + "byte" + " равно " + q);
+        short w = 32767;
+        System.out.println("Значение переменной w " + "с типом " + "byte" + " равно " + w);
+        int e = 2147483647;
+        System.out.println("Значение переменной e " + "с типом " + "byte" + " равно " + e);
+        long r = 13377410232145L;
+        System.out.println("Значение переменной r " + "с типом " + "byte" + " равно " + r);
+        float t = 1.0001f;
+        System.out.println("Значение переменной t " + "с типом " + "byte" + " равно " + t);
+        double y = 1.1234567;
+        System.out.println("Значение переменной y " + "с типом " + "byte" + " равно " + y);
+        }
+
+
+    public static void task2 () {
+        System.out.println("Задача 2");
+
+    }
+    public static void task3 () {
+        System.out.println("Задача 3");
+
+    }
+    public static void task4 () {
+        System.out.println("Задача 4");
+
+    }
+    public static void task5 () {
+        System.out.println("Задача 5");
+
+    }
+    public static void task6 () {
+        System.out.println("Задача 6");
+
+    }
+    public static void task7 () {
+        System.out.println("Задача 7");
+
+    }
+    public static void task8 () {
+        System.out.println("Задача 8");
+
+    }
+}
